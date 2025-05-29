06cb42ce22cbc677ed650852c72a314a45ef43c0
Mark-Asuncion <mark.asuncion.000@gmail.com>
2025-05-29 16:30:48 +0800

    Fix createBlog

--- src/Utils/Blog.tsx
+++ src/Utils/Blog.tsx
@@ -6,7 +6,14 @@ export type CreateBlog = {
     updated_at: Date,
     title: string,
     content: string,
-    images: string[]
+    images: string[] // data url of image
+};
+
+// images here is the name of the file in the public/bucket_name
+export interface Blog extends CreateBlog {
+    id: string,
+    user_id: string,
+    created_at: Date
 };
 
 export async function createBlog(
@@ -14,39 +21,50 @@ export async function createBlog(
     user: User,
     blog: CreateBlog
 ) {
-    const imgsNames = blog.images.map((im) => {
-        return `${btoa(im).substring(0,10)}`;
-    });
 
     // console.log(await dbContext.auth.getSession());
     // console.log(blog.images);
-    imgsNames.map(async (fname, i) => {
+    const imgsNames = [];
+
+    for (let i=0;i<blog.images.length;i++) {
         const dataUrl = blog.images[i];
         const spl = dataUrl.split(",")
         const contentType = spl[0].split(":")[1].split(";")[0];
         const data = Buffer.from(spl[1], "base64");
 
+        const fname = data.toString("base64").substring(0,10).replace(/\//g, "-");
+        imgsNames.push(fname);
+
         const res = await dbContext.storage.from("media")
-            .upload(`public/${fname}`, data, {
-                contentType: contentType,
-                upsert: true
-            });
+        .upload(`public/${fname}`, data, {
+            contentType: contentType,
+            upsert: true
+        });
 
         if (res.error) {
             throw res.error;
         }
-    });
+    }
 
-    const res = await dbContext.from("Blogs")
+    await dbContext.from("Blogs")
         .insert({
             updated_at: blog.updated_at,
             title: blog.title,
             content: blog.content,
             images: imgsNames,
             user_id: user.id
-        });
+        }).throwOnError();
+}
 
-    if (res.error) {
-        throw res.error;
-    }
+export async function getBlogs(
+    dbContext: DBContext,
+    offset: number = 0
+) {
+    const pageOffset = 5;
+    const ofs = offset*pageOffset;
+    const res = await dbContext.from("Blogs")
+        .select("*")
+        .order("updated_at", { ascending: false })
+        .range(0+ofs, pageOffset+ofs).throwOnError();
+    console.log(res.data);
 }
